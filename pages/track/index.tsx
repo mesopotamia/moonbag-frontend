f290144c41614be54a986047c7262895a47dde1c
Marwan Aziz <aziz.marwan@gmail.com>
2022-07-17 12:51:48 -0400

    add autocomplete

--- pages/track/index.tsx
+++ pages/track/index.tsx
@@ -8,6 +8,7 @@ import Select from 'react-select'
 export default function Index() {
     const [collection, setCollections] = useState<{slug: string}>({slug: ''});
     const [id, setId] = useState('otherdeed');
+    const [selection, setSelection] = useState([]);
     useEffect(() => {
         async function getData(id: string) {
             const response = await fetch(`https://collections.palmyra-flair01.workers.dev/api/collections/details/${id}`);
@@ -26,7 +27,10 @@ export default function Index() {
     return (    
         <>
             <Layout>
-                <Select options={options} />
+                <Select components={{ DropdownIndicator: null }}
+                        instanceId="123" controlShouldRenderValue={false}
+                        onChange={(item) => console.log(item)}
+                        isClearable={false} options={options} />
                 <h1>Collection: {collection.slug}</h1>
                 <CollectionItem></CollectionItem>
             </Layout>
