1d3739a2d3ed57eef436d0b72f5da464503461c1
Mesopotamia <aziz.marwan@gmail.com>
2022-07-17 08:35:49 -0400

    add fetch collection

--- pages/track/index.tsx
+++ pages/track/index.tsx
@@ -6,22 +6,22 @@ import CollectionItem from "../../components/collections/collection-item";
 
 
 export default function Index() {
-    const [collections, setCollections] = useState<{slug: string}>({slug: ''});
-    const id = "otherdeed";
+    const [collection, setCollections] = useState<{slug: string}>({slug: ''});
+    const [id, setId] = useState('otherdeed');
     useEffect(() => {
         async function getData(id: string) {
             const response = await fetch(`https://collections.palmyra-flair01.workers.dev/api/collections/details/${id}`);
-            const data = await response.json();
-            setCollections(data.data);
+            const jsonResponse = await response.json();
+            setCollections(jsonResponse.data);
         }
         getData(id);
-    }, [id]);
+    }, []);
        console.log('render');
-       console.log(collections.slug);
+       console.log(collection.slug);
     return (    
         <>
             <Layout>
-                <h1>Collection: {collections.slug}</h1>
+                <h1>Collection: {collection.slug}</h1>
                 <CollectionItem></CollectionItem>
             </Layout>
             
