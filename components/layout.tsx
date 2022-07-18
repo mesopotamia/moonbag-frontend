0f2716097a82f640e5750a178577b9055e58a76d
Mesopotamia <aziz.marwan@gmail.com>
2022-07-18 19:45:02 -0400

    add max width for desktop

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,15 +11,21 @@ export default function Layout({children}) {
                     <meta name="description" content="Track your NFTs across blockchains"/>
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
-                <header className="py-4 px-4 border-b-2 border-gray-100">
-                    BAYZED
+                <header className="p-4 border-b-2 border-gray-100">
+                    <div className="max-w-5xl m-auto">
+                        BAYZED
+                    </div>
                 </header>
-                <main className="h-full px-4 py-4">
-                    {children}
+                <main className="h-full p-4">
+                    <div className="max-w-5xl m-auto">{children}</div>
+                    
                 </main>
 
                 <footer className={`${styles.footer} border-t-2 border-gray-100 py-4 px-4`}>
-                    Bayzed.xyz
+                    <div className="max-w-5xl m-auto">
+                        Bayzed.xyz
+                    </div>
+                    
                 </footer>
             </div>
         </>
