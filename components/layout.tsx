f094856a56525de9131b1baf46345934c8c0de6e
Mesopotamia <aziz.marwan@gmail.com>
2022-07-19 14:17:13 -0400

    add mode switch

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,24 @@
 import Head from "next/head";
 import styles from '../styles/Home.module.css'
 import Logo from "./logo";
+import { MdNightlight } from "react-icons/md";
+import { useState } from "react";
+
+export type Lighting = 'light' | 'dark';
 
 export default function Layout({children}) {
+    const [lighting, setLighting] = useState<Lighting>('light');
+    const toggleLighting = () => {
+        if (lighting === 'light') {
+            setLighting('dark')
+        }
+        else {
+            setLighting('light');
+        }
+    }
     return (
         <>
-            <div className="h-screen flex flex-col dark bg-primary-bg-color">
+            <div className={`h-screen flex flex-col ${lighting} bg-primary-bg-color`}>
                 <Head>
                     <title>Bayzed</title>
                     <link rel="manifest" href="/manifest.json" />
@@ -13,8 +26,9 @@ export default function Layout({children}) {
                     <link rel="icon" href="/favicon.ico"/>
                 </Head>
                 <header className="p-4 border-b-2 border-gray-100">
-                    <div className="max-w-5xl m-auto">
+                    <div className="max-w-5xl m-auto flex justify-between">
                         <Logo />
+                        <button onClick={toggleLighting}><MdNightlight size="1.5em"/></button>
                     </div>
                 </header>
                 <main className="h-full p-4">
