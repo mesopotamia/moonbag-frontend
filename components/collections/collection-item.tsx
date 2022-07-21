9231cdc9d9f8a14a04f0b8dc0c59b055c1262e27
Mesopotamia <aziz.marwan@gmail.com>
2022-07-20 20:40:16 -0400

    fix build

--- components/collections/collection-item.tsx
+++ components/collections/collection-item.tsx
@@ -8,7 +8,7 @@ export default function CollectionItem({ collection }) {
 
                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                     aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </button>
             <div className="mb-2 flex items-center">
