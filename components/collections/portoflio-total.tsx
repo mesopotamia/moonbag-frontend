b11ccf637a2fca55dba1008df97a85541c5cd99e
Marwan Aziz <aziz.marwan@gmail.com>
2022-07-26 01:55:59 -0400

    switch img with Image

--- components/collections/portoflio-total.tsx
+++ components/collections/portoflio-total.tsx
@@ -1,11 +1,11 @@
+import Image from "next/image";
+
 export default function PortfolioTotal({amount}) {
     return (
         <div className="p-5 bg-accent-primary-color">
             <div className="text-white text-sm">Total</div>
             <div className="flex items-center">
-                <span>
-                    <img style={{width: "18px", height: "18px"}} src="/eth-currency.svg" alt="Ethereum Currency Symbol"/>
-                </span>
+                <Image width={18} height={18} src="/eth-currency.svg" alt="Ethereum Currency Symbol"/>
                 <span className="text-white ml-1 text-xl">{amount.toFixed(4)}</span>
             </div>
         </div>
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+
 export default function Logo () {
     return (
         <div className="flex items-center logo">
-            <img width="60px" src="/blockchain.svg" alt="Bayzed Logo"/>
+            <Image width={60} height={34} src="/blockchain.svg" alt="Bayzed Logo"/>
             <div className="font-black text-xl pl-2 ml-2 border-l text-primary-text-color">Bayzed</div>
         </div>
      )
