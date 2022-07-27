0e2ebecc448e5ac730af2a19bc887d7ff62d8ad9
Marwan Aziz <aziz.marwan@gmail.com>
2022-07-27 15:07:36 -0400

    change logo to svg

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,8 +3,12 @@ import Image from 'next/image';
 export default function Logo () {
     return (
         <div className="flex items-center logo">
-            <img width="30" src="/moonbag.svg" alt="Moonbag Logo"/>
-            <div className="font-black text-xl pl-2 ml-2 border-l text-primary-text-color">MOONBAG</div>
+            <div className="h-full flex items-center">
+                <img width="30" src="/moonbag.svg" alt="Moonbag Logo"/>
+            </div>
+            <div>
+                <img style={{top: 5}} width="140" src="/moonbag-text.svg" className="relative font-black text-xl text-primary-text-color" />
+            </div>
         </div>
      )
 }
