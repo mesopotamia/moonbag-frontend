7526e8239507c52929109b2ac4be912b73ca1c2d
Marwan Aziz <aziz.marwan@gmail.com>
2022-07-27 01:15:11 -0400

    change logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Logo () {
     return (
         <div className="flex items-center logo">
-            <Image width={60} height={34} src="/blockchain.svg" alt="Bayzed Logo"/>
-            <div className="font-black text-xl pl-2 ml-2 border-l text-primary-text-color">Bayzed</div>
+            <img width="30" src="/moonbag.svg" alt="Bayzed Logo"/>
+            <div className="font-black text-xl pl-2 ml-2 border-l text-primary-text-color">MOONBAG</div>
         </div>
      )
 }
