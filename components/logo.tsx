eb4a1a4a34422483cd24c9c3e959163c37ef5d81
Mesopotamia <aziz.marwan@gmail.com>
2022-08-01 16:28:31 -0400

    remove wrapping div

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,9 +3,7 @@ import Image from 'next/image';
 export default function Logo () {
     return (
         <div className="flex items-center logo">
-            <div>
                 <Image width="110" height="18" src="/moonbag-text.svg" className="relative font-black text-xl text-primary-text-color" />
-            </div>
         </div>
      )
 }
