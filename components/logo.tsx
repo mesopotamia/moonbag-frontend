bf1909e287a291ba6ea75cc5f6cb23b91dc2b54f
Mesopotamia <aziz.marwan@gmail.com>
2022-08-01 14:13:26 -0400

    same padding

--- components/logo.tsx
+++ components/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo () {
     return (
         <div className="flex items-center logo">
             <div>
-                <img width="110" src="/moonbag-text.svg" className="relative font-black text-xl text-primary-text-color" />
+                <Image width="110" height="18" src="/moonbag-text.svg" className="relative font-black text-xl text-primary-text-color" />
             </div>
         </div>
      )
