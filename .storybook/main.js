bd09d8c00b5aa98efd6ef92e734e7ced5b1767de
Marwan Aziz <aziz.marwan@gmail.com>
2022-08-05 01:26:58 -0400

    working with scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,25 @@
 module.exports = {
   "stories": [
+    "../**/*.stories.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)",
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: 'storybook-addon-sass-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+        rule: {
+          test: /\.(scss|sass)$/i,
+        },
+      },
+    },
   ],
   "framework": "@storybook/react",
   "core": {
