7f824c4ab0e7f43f2baf80f1733353f27eaf4906
Marwan Aziz <aziz.marwan@gmail.com>
2022-08-05 02:32:47 -0400

    Revert "Feature/test using story (#15)" (#17)
    This reverts commit 06e399a2ab62df77fc2ef7feb070d063518dbaf8.

--- components/common/dark-mode-toggle.stories.tsx
+++
@@ -1,6 +0,0 @@
-import DarkModeToggleComponent from "./dark-mode-toggle";
-
-export default {
-    component: DarkModeToggleComponent
-}
-export const DarkModeToggle = () => <DarkModeToggleComponent mode="light" />
--- src/pages/track/track.stories.tsx
+++
@@ -1,7 +0,0 @@
-import Track from './track';
-
-export default {
-    component: Track
-}
-
-export const TrackPage = () => <Track />
